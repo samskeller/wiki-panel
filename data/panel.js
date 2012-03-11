b0cadd826ed314390693b8fde332e40c7437bb1c
Sam Keller <samskeller@gmail.com>
2012-03-10 18:14:11 -0600

    changed another reddit name to a wiki name

--- data/panel.js
+++ data/panel.js
@@ -2,12 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// This is a content script.  It executes inside the context of the Reddit page
+// This is a content script.  It executes inside the context of the Wikipedia page
 // loaded into the panel and has access to that page's window object and other
 // global objects (although the page does not have access to globals defined by
 // this script unless they are explicitly attached to the window object).
 //
-// This content script is injected into the context of the Reddit page
+// This content script is injected into the context of the Wikipedia page
 // by the Panel API, which is accessed by the main add-on script in lib/main.js.
 // See that script for more information about how the panel is created.
 
