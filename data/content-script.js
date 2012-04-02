9a19220f4f034d26a9782a7bb0e0909e31c9d461
Sam Keller <samskeller@gmail.com>
2012-04-01 23:42:47 -0500

    The url of the website is now sent from the content script file to the main.js file using port

--- data/content-script.js
+++ data/content-script.js
@@ -1,3 +1,14 @@
-console.log($("object param").attr("value"));
+// Written by Sam Keller, April 1, 2012
+//
+// content-script.js
+//
+// A content script file for retrieving the url of the video on a given
+// khan academy website and then we can port this url to our main.js file
 
-$("object").replaceWith( "Sam Keller" );
+var url = ($("object param").attr("value"));
+//console.log(url);
+self.port.emit("myURL", url);
+
+
+// For replacing:
+//$("object").replaceWith( "Sam Keller" );
