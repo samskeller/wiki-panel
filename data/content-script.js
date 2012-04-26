d1153cc0caf4c0211920a2d50cd96ec1e300e292
Sam Keller <samskeller@gmail.com>
2012-04-25 22:15:20 -0500

    Almost getting the url of a website from the panel to pass to the server

--- data/content-script.js
+++ data/content-script.js
@@ -5,7 +5,8 @@
 // A content script file for retrieving the url of the video on a given
 // khan academy website and then we can port this url to our main.js file
 
-var url = ($("object param").attr("value"));
+var url = document.URL
+//var url = ($("object param").attr("value"));
 //console.log(url);
 self.port.emit("myURL", url);
 
